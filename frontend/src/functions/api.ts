579d5c284330ddbb1436d65fec0347b17bd6c67e
hentzrafael <rafaelhentz172@gmail.com>
2025-03-25 13:21:43 -0300

    fix: Removed normalizaUrl

--- frontend/src/functions/api.ts
+++ frontend/src/functions/api.ts
@@ -4,9 +4,3 @@ export async function httpGet(url: string) {
 	const response = await fetch(`${baseUrl}/${url}`);
 	return response.json();
 }
-
-function normalizaUrl(url: string) {
-	const protocolo = url.split("://")[0];
-	const restante = url.split("://")[1];
-	return `${protocolo}://${restante.replaceAll(/\/{2,}/g, "/")}`;
-}
